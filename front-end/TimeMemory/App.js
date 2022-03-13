bb12b6f0f735f847109d56c8f776588d3bbf3765
ten-3dev <mj03dev@gmail.com>
2022-03-14 00:16:53 +0900

    [FRONT] RN 폴더 RESET 및 베이스 작업 추후 다크모드 추가예정

--- front-end/TimeMemory/App.js
+++ front-end/TimeMemory/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
+import Main from './src/Screens';
 
 const App = () => {
   return (
-    <></>
+    <SafeAreaView style={{flex: 1}}>
+      <Main />
+    </SafeAreaView>
   );
 };
-
 
 export default App;
---
+++ front-end/TimeMemory/src/Screens/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styles from './style';
+
+const Main = () => {
+  return <Styles.Wrapper></Styles.Wrapper>;
+};
+
+export default Main;
