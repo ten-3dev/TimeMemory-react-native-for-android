8db7d54bdd3841dfeeac311f03373192f5158e79
ten-3dev <mj03dev@gmail.com>
2022-03-16 00:01:23 +0900

    [FRONT] Android Splash 추가, 추후 IOS Splash 추가 예정

--- front-end/TimeMemory/App.js
+++ front-end/TimeMemory/App.js
@@ -1,9 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import Main from './src/Screens';
 import Context from './Context';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide(); // Splash screen 종료
+  }, []);
+
   const [isDark, setIsDark] = useState(true);
 
   const contextSetting = {
