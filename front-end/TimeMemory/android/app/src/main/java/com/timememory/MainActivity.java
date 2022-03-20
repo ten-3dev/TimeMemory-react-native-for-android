1d960ef35b5fd63fec280f7ba9e7558780fe0b06
ten-3dev <mj03dev@gmail.com>
2022-03-20 14:54:22 +0900

    [FRONT] Full Screen false

--- front-end/TimeMemory/android/app/src/main/java/com/timememory/MainActivity.java
+++ front-end/TimeMemory/android/app/src/main/java/com/timememory/MainActivity.java
@@ -9,7 +9,7 @@ import org.devio.rn.splashscreen.SplashScreen;
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState){
-    SplashScreen.show(this, true);
+    SplashScreen.show(this);
     super.onCreate(savedInstanceState);
   }
 
