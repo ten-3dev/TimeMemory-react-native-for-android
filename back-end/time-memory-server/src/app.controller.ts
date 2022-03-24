ad709edb1f11e7dc72e3626f3244de9478ffbc10
ten-3dev <mj03dev@gmail.com>
2022-03-24 15:12:11 +0900

    [BACK] URL 연결

--- back-end/time-memory-server/src/app.controller.ts
+++ back-end/time-memory-server/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('getAll')
+  getAll(){
+      return "getAll"
+  }
+
+  @Post('create')
+  create(){
+      return "create"
+  }
+
+  @Delete('remove/:id')
+  remove(){
+      return "remove"
   }
 }
