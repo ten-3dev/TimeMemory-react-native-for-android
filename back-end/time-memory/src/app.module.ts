7b7b7632db54a0a457e68e7e0c7c02990e1698c5
ten-3dev <mj03dev@gmail.com>
2022-03-25 23:56:39 +0900

    [BACK] env로 몽고DB url 환경변수 설정

--- back-end/time-memory/src/app.module.ts
+++ back-end/time-memory/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MongooseModule} from '@nestjs/mongoose'
 import { AppSchema } from './app.model';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://minjae:dldjvhs03@cluster0.iskle.mongodb.net/time-memory?retryWrites=true&w=majority'), 
-    MongooseModule.forFeature([{name:"App", schema:AppSchema}])
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    MongooseModule.forFeature([{name:"App", schema:AppSchema}]),
   ],
   controllers: [AppController],
   providers: [AppService],
